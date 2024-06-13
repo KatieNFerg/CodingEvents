a9d7d59f121a403ffc1c1aa13c274f19ab1e7f83
KatieNFerg <ketchersidekatie@gmail.com>
2024-06-13 13:39:45 -0500

    studio17

---
+++ CodingEvents/Models/EventCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace CodingEvents;
+
+public class EventCategory
+{
+public int Id { get; set; }
+public string? Name { get; set; }
+
+public EventCategory()
+{}
+
+public EventCategory(string name)
+{
+    Name = Name;
+}
+}
+
---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels;
+
+
+public class AddEventCategoryViewModel
+{
+    [Required(ErrorMessage = "Please add an event category name")]
+    [StringLength(20, MinimumLength =3, ErrorMessage ="Category name must be between 3-20 characters")]
+    public string? Name { get; set; }
+}
