b4ec8b07e4e6a7246eea27f4b592c77ff0379395
KatieNFerg <ketchersidekatie@gmail.com>
2024-06-13 10:40:00 -0500

    myStudio17

--- CodingEvents/ViewModels/AddEventCategoryViewModel.cs
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -5,8 +5,8 @@ namespace CodingEvents.ViewModels
 {
 	public class AddEventCategoryViewModel
 	{
-		[Required(ErrorMessage ="Please add a category type")]
-		[StringLength(20, MinimumLength =1, ErrorMessage ="Category type should be between 1-20 characters")]
+		[Required(ErrorMessage ="Please add an event category name")]
+		[StringLength(20, MinimumLength =3, ErrorMessage ="Category name should be between 3-20 characters")]
 		public string? Name { get; set; }
 	}
 }
